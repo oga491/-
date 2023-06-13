8f7414fb26389f29d32403c5fda1ca57e3230e6b
oga491 <134859974+oga491@users.noreply.github.com>
2023-06-13 18:02:59 +0900

    あ

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import streamlit as st
 import json
 
 
-def save_post(post):
+def save_post(title, content):
+    post = {"title": title, "content": content}
     with open('posts.json', 'a') as file:
         json.dump(post, file)
         file.write('\n')
@@ -15,21 +16,23 @@ def load_posts():
 
 
 def main():
-    st.title("三重野　寿司")
+    st.title("掲示板アプリ")
 
     # 新規投稿の入力
-    new_post = st.text_area("新規投稿", height=100)
+    new_post_title = st.text_input("タイトル")
+    new_post_content = st.text_area("新規投稿", height=100)
 
     # 投稿ボタンが押された場合
-    if st.button("投稿する") and new_post:
-        save_post(new_post)
+    if st.button("投稿する") and new_post_title and new_post_content:
+        save_post(new_post_title, new_post_content)
         st.success("投稿が保存されました！")
 
     # 保存された投稿の表示
     posts = load_posts()
     st.subheader("保存された投稿")
     for post in posts:
-        st.text(post)
+        st.text(post["title"])
+        st.text(post["content"])
         st.markdown("---")
 
 
