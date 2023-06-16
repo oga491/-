f2458c33b124e18dfd85bb14e0037498fc0dbb97
oga491 <134859974+oga491@users.noreply.github.com>
2023-06-16 14:30:29 +0900

    ｆ

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-
 import streamlit as st
 import json
+from datetime import datetime
 
-# 禁止ワードのリスト
-banned_words = ["馬鹿", "禁止ワード2", "禁止ワード3"]
-
-# ユーザーの投稿内容をチェックする関数
-def check_post_content(title, content):
-    # タイトルと投稿内容の禁止ワードの検出
-    for banned_word in banned_words:
-        if banned_word in title:
-            title = title.replace(banned_word, "＠" * len(banned_word))
-        if banned_word in content:
-            content = content.replace(banned_word, "＠" * len(banned_word))
-    return title, content
+# ...
 
 def save_post(title, content):
-    post = {"title": title, "content": content}
+    post = {
+        "title": title,
+        "content": content,
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # 投稿された時刻を追加
+    }
     with open('posts.json', 'a') as file:
         json.dump(post, file)
         file.write('\n')
@@ -28,18 +21,7 @@ def load_posts():
 def main():
     st.title("掲示板アプリ")
 
-    # 新規投稿の入力
-    new_post_title = st.text_input("タイトル")
-    new_post_content = st.text_area("新規投稿", height=100)
-
-    # 投稿ボタンが押された場合
-    if st.button("投稿する") and new_post_title and new_post_content:
-        new_post_title, new_post_content = check_post_content(new_post_title, new_post_content)
-        if "＠" in new_post_title or "＠" in new_post_content:
-            st.warning("禁止ワードが含まれています！")
-
-        save_post(new_post_title, new_post_content)
-        st.success("投稿が保存されました！")
+    # ...
 
     # 保存された投稿の表示
     posts = load_posts()
@@ -49,8 +31,10 @@ def main():
         st.info("まだ投稿がありません。")
     else:
         for post in posts:
-            st.text(post["title"])
-            st.text(post["content"])
+            st.text(f"タイトル: {post['title']}")
+            st.text(f"投稿内容: {post['content']}")
+            if "timestamp" in post:
+                st.text(f"投稿時刻: {post['timestamp']}")  # 投稿された時刻を表示
             st.markdown("---")
 
 if __name__ == "__main__":
