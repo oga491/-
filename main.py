d2464c147d51f57131ef728fb58e65223c66160d
oga491 <134859974+oga491@users.noreply.github.com>
2023-10-25 16:12:53 +0900

    しゅんた

--- main.py
+++ main.py
@@ -1,58 +1,49 @@
+import streamlit as st
 import json
+import pandas as pd
 from datetime import datetime
 import pytz
 import urllib.parse
-import streamlit as st
 
 # 禁止ワードのリスト
-banned_words = ["馬鹿", "禁止ワード2", "禁止ワード3"]
+# Excelファイルから読み込む
+def load_banned_words():
+    df = pd.read_csv("禁止ワード.csv")
+    return df['禁止用語'].tolist()
 
-# 投稿を管理するクラス
-class Post:
-    def __init__(self, title, content):
-        self.title = title
-        self.content = content
-        self.timestamp = datetime.now(pytz.timezone("Asia/Tokyo")).strftime("%Y-%m-%d %H:%M:%S")
-        self.good_count = 0  # グッドのカウント
-        self.bad_count = 0   # バッドのカウント
+banned_words = load_banned_words()
 
-    def to_dict(self):
-        return {
-            "title": self.title,
-            "content": self.content,
-            "timestamp": self.timestamp,
-            "good_count": self.good_count,  # グッドのカウントを保存
-            "bad_count": self.bad_count,    # バッドのカウントを保存
-        }
-
-# 各投稿を格納するリスト
-posts = []
-
+# ユーザーの投稿内容をチェックする関数
 def check_post_content(title, content):
-    # 禁止ワードが含まれているかチェック
-    for word in banned_words:
-        if word in title or word in content:
-            st.warning("禁止ワードが含まれています！")
-            return None
-    return Post(title, content)
+    # タイトルと投稿内容の禁止ワードの検出
+    for banned_word in banned_words:
+        if banned_word in title:
+            title = title.replace(banned_word, "＠" * len(banned_word))
+        if banned_word in content:
+            content = content.replace(banned_word, "＠" * len(banned_word))
+    return title, content
 
-def save_post(post):
+# 以下のコードは変更なし
+def save_post(title, content):
+    now = datetime.now(pytz.timezone("Asia/Tokyo"))
+    now_str = now.strftime("%Y-%m-%d %H:%M:%S")
+    post = {"title": title, "content": content, "timestamp": now_str}
     with open('posts.json', 'a') as file:
-        file.write(json.dumps(post.to_dict()))
+        file.write(json.dumps(post))
         file.write('\n')
 
 def load_posts():
-    loaded_posts = []
     with open('posts.json', 'r') as file:
         lines = file.readlines()
-        for line in lines:
-            post_dict = json.loads(line.strip())
-            post = Post(post_dict["title"], post_dict["content"])
-            post.timestamp = post_dict["timestamp"]
-            post.good_count = post_dict.get("good_count", 0)  # グッドのカウントをロード
-            post.bad_count = post_dict.get("bad_count", 0)    # バッドのカウントをロード
-            loaded_posts.append(post)
-    return loaded_posts
+        posts = [json.loads(line.strip()) for line in lines]
+        
+        # タイムスタンプを日本時間に変換
+        for post in posts:
+            timestamp = datetime.strptime(post['timestamp'], "%Y-%m-%d %H:%M:%S")
+            timestamp = pytz.timezone("Asia/Tokyo").localize(timestamp)
+            post['timestamp'] = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+        return posts
 
 def main():
     st.title("掲示板アプリ")
@@ -60,40 +51,27 @@ def main():
     # 新規投稿の入力
     new_post_content = st.text_area("管理者以外記述厳禁", height=100)
     new_post_title = st.text_input("ページ")
-
+    
     # 投稿ボタンが押された場合
     if st.button("投稿する") and new_post_title and new_post_content:
-        new_post = check_post_content(new_post_title, new_post_content)
-        if new_post:
-            posts.append(new_post)
-            save_post(new_post)
+        new_post_title, new_post_content = check_post_content(new_post_title, new_post_content)
+        if "＠" in new_post_title or "＠" in new_post_content:
+            st.warning("禁止ワードが含まれています！")
+        else:
+            save_post(new_post_title, new_post_content)
+            st.success("投稿が保存されました！")
 
-    # 投稿一覧を表示
-    loaded_posts = load_posts()
-    if not loaded_posts:
+    # 保存された投稿の表示
+    posts = load_posts()
+    st.subheader("保存された投稿")
+
+    if not posts:
         st.info("まだ投稿がありません。")
     else:
-        for i, post in enumerate(loaded_posts, start=1):
+        for post in posts:
             # 各タイトルにリンクを付けて表示
-            post_url = f"<a href='https://maichan-bord-{urllib.parse.quote(post.title)}.streamlit.app'>{post.title}</a>"
-            st.subheader(f"{i}. {post.content}")
-            st.write(post.timestamp)  # タイムスタンプを表示
-
-            # GoodボタンとBadボタンを追加
-            col1, col2 = st.columns(2)
-            good_button = col1.button(f"Good ({post.good_count})", key=f"good_{post.title}")
-            bad_button = col2.button(f"Bad ({post.bad_count})", key=f"bad_{post.title}")
-            if good_button:
-                post.good_count += 1  # グッドのカウントを増やす
-            if bad_button:
-                post.bad_count += 1    # バッドのカウントを増やす
-
-            # 評価カウンターを表示
-            st.write(f"Good: {post.good_count}, Bad: {post.bad_count}")
-
-            st.markdown(post_url, unsafe_allow_html=True)
-            st.markdown("---")
-
-if __name__ == "__main__":
-    main()
-
+            post_url = f"<a href='https://maichan-bord-{urllib.parse.quote(post['title'])}.streamlit.app'>{post['title']}</a>"
+            st.subheader(post['title'])
+            st.write(post['content'])  # 投稿内容
+            st.write(post['timestamp'])  # タイムスタンプ
+            st
