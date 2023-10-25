2531ba54886bffd6678590741f68d54d11331f49
oga491 <134859974+oga491@users.noreply.github.com>
2023-10-25 16:22:15 +0900

    ｙふぉうｙふぉ

--- main.py
+++ main.py
@@ -1,34 +1,3 @@
-import streamlit as st
-import json
-import pandas as pd
-from datetime import datetime
-import pytz
-import urllib.parse  # urllib.parseモジュールをインポート
-
-
-
-# 以下のコードは変更なし
-def save_post(title, content):
-    now = datetime.now(pytz.timezone("Asia/Tokyo"))
-    now_str = now.strftime("%Y-%m-%d %H:%M:%S")
-    post = {"title": title, "content": content, "timestamp": now_str}
-    with open('posts.json', 'a') as file:
-        file.write(json.dumps(post))
-        file.write('\n')
-
-def load_posts():
-    with open('posts.json', 'r') as file:
-        lines = file.readlines()
-        posts = [json.loads(line.strip()) for line in lines]
-        
-        # タイムスタンプを日本時間に変換
-        for post in posts:
-            timestamp = datetime.strptime(post['timestamp'], "%Y-%m-%d %H:%M:%S")
-            timestamp = pytz.timezone("Asia/Tokyo").localize(timestamp)
-            post['timestamp'] = timestamp.strftime("%Y-%m-%d %H:%M:%S")
-
-        return posts
-
 def main():
     st.title("掲示板アプリ")
 
@@ -38,12 +7,15 @@ def main():
     
     # 投稿ボタンが押された場合
     if st.button("投稿する") and new_post_title and new_post_content:
-        new_post_title, new_post_content = check_post_content(new_post_title, new_post_content)
-        if "＠" in new_post_title or "＠" in new_post_content:
-            st.warning("禁止ワードが含まれています！")
-        else:
-            save_post(new_post_title, new_post_content)
-            st.success("投稿が保存されました！")
+        try:
+            new_post_title, new_post_content = check_post_content(new_post_title, new_post_content)
+            if "＠" in new_post_title or "＠" in new_post_content:
+                st.warning("禁止ワードが含まれています！")
+            else:
+                save_post(new_post_title, new_post_content)
+                st.success("投稿が保存されました！")
+        except Exception as e:
+            st.error(f"エラーが発生しました: {e}")
 
     # 保存された投稿の表示
     posts = load_posts()
@@ -59,6 +31,3 @@ def main():
             st.write(post['content'])  # 投稿内容
             st.write(post['timestamp'])  # タイムスタンプ
             st.markdown(post_url, unsafe_allow_html=True)  # リンクを表示
-
-if __name__ == "__main__":
-    main()
