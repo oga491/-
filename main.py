01c8059c918b8829ef1e96f9ef674d739163b3fa
oga491 <134859974+oga491@users.noreply.github.com>
2023-11-10 13:46:10 +0900

    ggd

--- main.py
+++ main.py
@@ -4,15 +4,11 @@ import pandas as pd
 from datetime import datetime
 import pytz
 import urllib.parse
-
-
 # 禁止ワードをExcelファイルから読み込む
 df = pd.read_excel("banned_list.xlsx", sheet_name=0)
 #禁止ワードをbanned_words に
 banned_words = df['禁止ワード'].tolist()
 banned_words = [str(word) for word in banned_words]
-
-
 # ユーザーの投稿内容をチェックする関数
 def check_post_content(title, content):
     
@@ -22,7 +18,6 @@ def check_post_content(title, content):
         if banned_word in content:
             content = content.replace(banned_word, "＠" * len(banned_word))# タイトルと投稿内容の禁止ワードの検出し、禁止ワードがあったら文字を＠に変換
     return title, content
-
     
 def save_post(title, content):
     #タイムスタンプを設定
@@ -32,7 +27,6 @@ def save_post(title, content):
     with open('posts.json', 'a') as file:
         file.write(json.dumps(post))
         file.write('\n')
-
 def load_posts():
     with open('posts.json', 'r') as file:
         lines = file.readlines()
@@ -42,13 +36,10 @@ def load_posts():
             timestamp = datetime.strptime(post['timestamp'], "%Y-%m-%d %H:%M:%S")
             timestamp = pytz.timezone("Asia/Tokyo").localize(timestamp)
             post['timestamp'] = timestamp.strftime("%Y-%m-%d %H:%M:%S")
-
     return posts
        
-
 def main():
     st.title("掲示板アプリ")
-
     # ページのタイトルの入力
     new_post_content = st.text_area("管理者以外記述厳禁", height=100)
     new_post_title = st.text_input("ページ")
@@ -58,7 +49,6 @@ def main():
         new_post_title, new_post_content = check_post_content(new_post_title, new_post_content)
         if "＠" in new_post_title or "＠" in new_post_content:
             st.warning("禁止ワードが含まれています！")
-
         save_post(new_post_title, new_post_content)
         st.success("投稿が保存されました！")
 #↑まで入力の段
@@ -74,7 +64,4 @@ def main():
             st.subheader(post['content'])
             st.write(post['timestamp'])  # タイムスタンプ
             st.markdown(post_url, unsafe_allow_html=True)
-            st.markdown("---")
-
-if __name__ == "__main__":
-    main()
+	
